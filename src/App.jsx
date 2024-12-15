424902529c8796c2082f34dbd7e9c6b01fd86068
Artur Luniaka <artur01049718@gmail.com>
2024-12-15 17:06:52 +0100

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const CartPage = lazy(() => import("./pages/CartPage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
+const ProductsDetailPage = lazy(() => import("./pages/ProductDetailsPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="catalog/:productId" element={<ProductsDetailPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
