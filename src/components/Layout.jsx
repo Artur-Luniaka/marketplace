083ba8f47b4e97767f194178ae7a728d813505d8
Artur Luniaka <artur01049718@gmail.com>
2024-12-15 18:08:49 +0100

    added own fonts/cheked

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,15 @@
-const Layout = () => {
-  return <div></div>;
+import { Suspense } from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Suspense fallback={null}>{children}</Suspense>
+      </main>
+      <footer></footer>
+    </>
+  );
 };
 
 export default Layout;
