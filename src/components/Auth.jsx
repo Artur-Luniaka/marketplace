17bcbf872993ed79fc36e529ba744a590ea59ebd
Artur Luniaka <artur01049718@gmail.com>
2024-12-17 20:21:02 +0100

    set auth size/set container

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,17 +4,17 @@ import { CgLogIn } from "react-icons/cg";
 
 const Auth = () => {
   return (
-    <div className="flex xxs:flex-col xxs:gap-2 xs:flex-row xs:gap-3">
+    <div className="flex xs:flex-col xs:gap-1 md:flex-row md:gap-3">
       <Link
         to="register"
-        className="text-2xl text-3 font-gambetta flex gap-1 place-items-center"
+        className="text-lg text-3 font-gambetta flex gap-1 place-items-center"
       >
         <TiUserAddOutline className="text-3" />
         Registration
       </Link>
       <Link
         to="login"
-        className="text-2xl text-3 font-gambetta flex gap-1 place-items-center"
+        className="text-lg text-3 font-gambetta flex gap-1 place-items-center"
       >
         <CgLogIn className="text-3" />
         Login
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,15 @@ export default {
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem", // Отступы по умолчанию
-          xxs: "0.5rem", // Отступы для экранов >= 320px
-          xs: "1rem", // Отступы для экранов >= 480px
-          sm: "2rem", // Отступы для экранов >= 640px
-          md: "4rem", // Отступы для экранов >= 768px
-          lg: "6rem", // Отступы для экранов >= 1024px
-          xl: "8rem", // Отступы для экранов >= 1280px
+          xs: "16px",
+          md: "16px",
+          xl: "20px",
         },
       },
     },
     screens: {
-      xxs: "320px",
-      xs: "480px",
-      sm: "640px",
+      xs: "320px",
       md: "768px",
-      lg: "1024px",
       xl: "1280px",
     },
   },
