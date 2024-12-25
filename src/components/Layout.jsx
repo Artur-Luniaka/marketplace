9e0e1cbe5d9f986e5959a297552314da9da6c321
Artur Luniaka <artur01049718@gmail.com>
2024-12-25 13:52:46 +0100

    add Hero component(unlog and unreg version)/set styles for him

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 const Layout = ({ children }) => {
   return (
     <div className="container mx-auto py-1 bg-1 text-1">
-      <header className="xs:items-center bg-3 p-3.5 flex justify-between rounded-lg">
+      <header className="xs:items-center bg-3 p-3.5 flex justify-between rounded-lg mb-2.5">
         <Navigation />
       </header>
       <main>
