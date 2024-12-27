9bc8da42fbec37793aaf658573f435ad208c6fb4
Artur Luniaka <artur01049718@gmail.com>
2024-12-27 09:15:07 +0100

    added redax folder(set only goods folder)/added NewArrivals comp(withoutMarUp)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import NewArrivals from "../components/NewArrivals";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <NewArrivals />
     </>
   );
 };
