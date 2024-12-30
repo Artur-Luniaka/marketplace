e54d6ed010ad44e57b1705716dbb485fc67b9760
Artur Luniaka <artur01049718@gmail.com>
2024-12-30 09:50:52 +0100

    added pagination for NewArrivals component(not finished)

--- src/redux/goods/operations.js
+++ src/redux/goods/operations.js
@@ -9,7 +9,18 @@ export const fetchGoods = createAsyncThunk(
   "goods/fetchAll",
   async (_, thunkApi) => {
     try {
-      const response = await apiGoods("/products");
+      const response = await apiGoods("/products?limit=9");
+      return response.data.products;
+    } catch (e) {
+      return thunkApi.rejectWithValue(e.message);
+    }
+  }
+);
+export const fetchMoreGoods = createAsyncThunk(
+  "goods/fetchMoreGoods",
+  async (skip, thunkApi) => {
+    try {
+      const response = await apiGoods(`/products?limit=9&skip=${skip}`);
       return response.data.products;
     } catch (e) {
       return thunkApi.rejectWithValue(e.message);
--- src/redux/goods/slice.js
+++ src/redux/goods/slice.js
@@ -1,17 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchGoods } from "./operations";
+import { fetchGoods, fetchMoreGoods } from "./operations";
 
 const initialState = {
   goods: [],
+  skip: 9,
 };
 
 const slice = createSlice({
   name: "goods",
   initialState,
   extraReducers: (builder) => {
-    builder.addCase(fetchGoods.fulfilled, (state, action) => {
-      state.goods = action.payload;
-    });
+    builder
+      .addCase(fetchGoods.fulfilled, (state, action) => {
+        state.goods = action.payload;
+      })
+      .addCase(fetchMoreGoods.fulfilled, (state, action) => {
+        state.goods.push(...action.payload);
+        state.skip += 9;
+      });
   },
 });
 
